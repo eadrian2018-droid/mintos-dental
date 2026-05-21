245e2cb8b78bf67e1b314f8e0da9de828c133dc9
Edgar <eadrian2018@gmail.com>
2026-05-21 09:16:45 -0700

    stable qr working

--- client/src/components/QRCodePaciente.tsx
+++ client/src/components/QRCodePaciente.tsx
@@ -1,33 +1,14 @@
 import QRCode from "react-qr-code";
 
-const QRCodePaciente = () => {
+export default function QRCodePaciente() {
 
   const urlFormulario =
     "https://mintos-dental-q77v-4w0x2ube6-eadrian2018-droids-projects.vercel.app/";
 
   return (
-    <div
-      className="
-        bg-white
-        rounded-3xl
-        shadow-xl
-        p-8
-        mb-10
-        flex
-        flex-col
-        items-center
-      "
-    >
+    <div className="bg-white rounded-3xl shadow-xl p-8 mb-10 flex flex-col items-center">
 
-      <h2
-        className="
-          text-2xl
-          font-bold
-          text-teal-700
-          mb-6
-          text-center
-        "
-      >
+      <h2 className="text-2xl font-bold text-teal-700 mb-6 text-center">
         Escanee para llenar historial clínico
       </h2>
 
@@ -36,13 +17,7 @@ const QRCodePaciente = () => {
         size={220}
       />
 
-      <p
-        className="
-          mt-6
-          text-gray-600
-          text-center
-        "
-      >
+      <p className="mt-6 text-gray-600 text-center">
         Escanee desde su celular
         <br />
         para llenar su historial clínico
@@ -50,6 +25,4 @@ const QRCodePaciente = () => {
 
     </div>
   );
-};
-
-export default QRCodePaciente;
+}
